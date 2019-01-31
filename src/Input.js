8e9d860b3c0c41d602127daadeefee952a5d7bb1
Alana Farkas <alana.farkas@zocdoc.com>
2019-01-31 15:29:32 -0500

    feat: add title and input component

---
+++ src/Input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Input = (props) => {
+    
+    const styles = {
+        margin: 50,
+    }
+
+    return (
+        <form style={styles}>
+            <input type='text' />
+        </form>
+    )
+}
+
+export default Input;
