45d7863b2464dc3bc29a443e86da68e5a2bc02f7
Alana Farkas <alana.farkas@zocdoc.com>
2019-01-31 16:28:12 -0500

    feat: styles icon inside input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import calendar_icon from './assets/calendar_icon.svg';
 import Input from './Input'
+import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <h1>Choose a date</h1>
+      <InputContainerDiv>      
         <Input />
+        <CalendarImg src={calendar_icon} />
+      </InputContainerDiv>
       </div>
     );
   }
 }
 
 export default App;
+
+const InputContainerDiv = styled.div`
+  display: inline-block;
+  position: relative;
+`;
+
+const CalendarImg = styled.img`
+  height: 25px;
+  position: absolute;
+  right: 3px;
+  top: 3px;
+`;
--- src/Input.js
+++ src/Input.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Input = (props) => {
-    
-    const styles = {
-        margin: 50,
-    }
-
     return (
-        <form style={styles}>
-            <input type='text' />
-        </form>
+        <TimeDateInput type='text' />
     )
 }
 
 export default Input;
+
+const TimeDateInput = styled.input`
+    height: 25px;
+    width: 200px;
+`;
