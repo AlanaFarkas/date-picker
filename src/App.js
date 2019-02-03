05462a9ea0307542ac084d0470b49491daeb2f98
Alana Farkas <alana.farkas@zocdoc.com>
2019-02-03 16:27:49 -0500

    feat: changes table to divs

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import calendar_icon from './assets/calendar_icon.svg';
 import Input from './Input'
 import styled from 'styled-components';
 import CalendarContainer from './CalendarContainer';
+import { ALL_MONTHS, ALL_DAYS, TODAY } from './constants';
 
 class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      openCalendar: false
+      openCalendar: false,
     }
   }
 
@@ -23,13 +24,21 @@ class App extends Component {
 
   render() {
 
+    let month = TODAY.getMonth();
+    let monthString = ALL_MONTHS[month];
+    
+    const dayStrings = ALL_DAYS.map((day, i) => {
+        return <td key={i}>{day}</td>
+    });
+
+
     return (
       <div className="App">
       <h1>Choose a date</h1>
       <InputContainerDiv>      
         <Input />
         <CalendarImg onClick={this.handleClick} src={calendar_icon} />
-        {this.state.openCalendar ? <CalendarContainer/> : null}
+        {this.state.openCalendar ? <CalendarContainer month={monthString} days={dayStrings} /> : null}
       </InputContainerDiv>
       </div>
     );
--- src/CalendarContainer.js
+++ src/CalendarContainer.js
@@ -1,42 +1,33 @@
 import React from 'react';
-import { ALL_MONTHS, ALL_DAYS, TODAY } from './constants';
 import styled from 'styled-components';
 
-const CalendarContainer = (props) => {
-
-    let month = TODAY.getMonth();
-    let monthString = ALL_MONTHS[month];
-    
-    const dayStrings = ALL_DAYS.map((day, i) => {
-        return <td key={i}>{day}</td>
-    });
-
+const CalendarContainer = ({month, days, handleNext, handlePrev}) => {
 
     return (
-    <CalendarTable>
-        <tr>
-            <th>{monthString}</th>
-        </tr>
-        <tbody>
-            <tr>
-                {dayStrings}
-            </tr>
-            <tr>
-                {/* <td>Eve</td>
-                <td>Jackson</td> 
-                <td>94</td> */}
-            </tr>
-        </tbody>
-    </CalendarTable>
+    <Calendar>
+        <MonthContainer>
+            <button onClick={handlePrev}>Prev</button>
+            <Month>{month}</Month>
+            <button onClick={handleNext}>Next</button>
+        </MonthContainer>
+        <DaysContainer>
+            <p>{days}</p>
+        </DaysContainer>
+    </Calendar>
     )
 }
 
 export default CalendarContainer;
 
-const CalendarTable = styled.table`
-    width: 100%;
-    border: 1px black solid;
-    th {
-        font-size: 25px;
-    }
+const Calendar = styled.div`
+    border: 1px red solid;
+`;
+
+const MonthContainer = styled.div`
+`;
+
+const Month = styled.h2`
+`;
+
+const DaysContainer = styled.div`
 `;
