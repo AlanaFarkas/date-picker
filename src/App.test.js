fa3a2600c4de92923b65f6983f4751477c17d2b9
Alana Farkas <alana.farkas@zocdoc.com>
2019-02-08 09:51:23 -0500

    feat: starts testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { expect, shallow } from 'enzyme';
+import CalendarContainer from './CalendarContainer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+describe('<CalendarContainer />', () => {
+  it('renders a <CalendarComponent /> when the calendar icon is clicked', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<CalendarContainer />, div);
+  })
+})
