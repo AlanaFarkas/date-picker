b607a89be81d0d2c2c90b3b0efaaf063bb251acb
Alana Farkas <alana.farkas@zocdoc.com>
2019-05-07 17:37:14 -0400

    test: two passing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { expect, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import Calendar from './Calendar';
 
 describe('App.js', () => {
   it('does not render calendar component on page load', () => {
     const wrapper = shallow(<App />)
-    console.log(wrapper.debug());
-    expect(wrapper.exists(<Calendar />)).to.equal(false);
-  })
+    expect(wrapper.exists(<Calendar />)).toEqual(false);
+  });
 
-  // describe('<Calendar />', () => {
-  //   it('renders a <Calendar /> when the calendar icon is clicked', () => {
-  //     const wrapper = shallow(<Calendar />)
-  //     console.log(wrapper.debug());
-  //   })
-  // })
+  it('renders the correct header message', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toEqual('Choose a date');
+  });
+
 })
 
