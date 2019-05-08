b0f4c026ab0011e059f7d14e7dbf429710ddcda2
Alana Farkas <alana.farkas@zocdoc.com>
2019-05-08 15:02:41 -0400

    test: confirm calendar image icon is present

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ describe('App.js', () => {
   it('renders the correct header message', () => {
     const wrapper = mountWithCustomWrappers(<App />);
     //not super sure why this is working -- when I look in the console I see a lot of html output that looks correct but no actual matching text
-    console.log(wrapper.findByDataTest('app-header').at(0).debug())
+    // console.log(wrapper.findByDataTest('app-header').at(0).debug())
     expect(wrapper.findByDataTest('app-header').exists()).toBe(true);
     expect(wrapper.findByDataTest('app-header').at(0).text()).toEqual('Choose a date');
   });
@@ -15,6 +15,15 @@ describe('App.js', () => {
     const wrapper = mountWithCustomWrappers(<App />);
     expect(wrapper.findByDataTest('input-field').exists()).toBe(true);
   })
+
+  it('renders the calendar image icon component', () => {
+    const wrapper = mountWithCustomWrappers(<App />);
+    expect(wrapper.findByDataTest('calendar-icon').exists()).toBe(true);
+  })
+
+  // it('renders the date picker when the calendar image icon is clicked', () => {
+  //     const wrapper = mountWithCustomWrappers(<App />);
+  // })
 
   it('does not render calendar component on page load', () => {
     const wrapper = mountWithCustomWrappers(<App />);
